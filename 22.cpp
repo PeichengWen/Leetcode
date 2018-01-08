b6c33a0bd17bb659606f0f129352b428f13fbf10
PeichengWen <pwen@troy.edu>
2018-01-07 19:29:29 -0600

    code
    wrong output

--- 22.cpp
+++ 22.cpp
@@ -1,1 +1,24 @@
-
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> v;
+        string s;
+        add(v,s,n,n);
+        return v;
+        
+    }
+    void add(vector<string> &v,string s,int m,int n){
+        if(m==0&&n==0){
+             v.push_back(s);
+            return;
+        }
+        if(m>0){
+            s+='(';
+            add(v,s,m-1,n);
+        }
+        if(n>m){
+            s+=')';
+            add(v,s,m,n-1);
+        }
+    }
+};
