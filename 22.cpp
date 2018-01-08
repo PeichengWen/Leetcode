a85bb7f376c239bb560b2794e2b84c8b480378e3
PeichengWen <pwen@troy.edu>
2018-01-07 19:37:10 -0600

    accepted
    fix the parameter

--- 22.cpp
+++ 22.cpp
@@ -9,16 +9,16 @@ public:
     }
     void add(vector<string> &v,string s,int m,int n){
         if(m==0&&n==0){
-             v.push_back(s);
+            v.push_back(s);
             return;
         }
         if(m>0){
-            s+='(';
-            add(v,s,m-1,n);
+            //s+='(';
+            add(v,s+"(",m-1,n);
         }
         if(n>m){
-            s+=')';
-            add(v,s,m,n-1);
+            //s+=')';
+            add(v,s+")",m,n-1);
         }
     }
 };
