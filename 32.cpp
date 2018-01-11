fb2c58d94c48dab9c9122513eceb66f95056d395
PeichengWen <pwen@troy.edu>
2018-01-11 12:46:42 -0600

    accepted

--- 32.cpp
+++ 32.cpp
@@ -3,17 +3,18 @@ public:
     int longestValidParentheses(string s) {
         int n=s.size();
         int lenth = 0, maxlenth = 0;
+        int start = -1;
         stack<int> a;
         for(int i=0;i<n;i++){
             lenth = 0;
             if(s[i]=='('){
                 a.push(i);
             }else if(s[i]==')'){
-                if(a.empty()) continue;
+                if(a.empty()) start = i;
                 else{
                     a.pop();
                     if(a.empty())
-                        lenth = i+1;
+                        lenth = i-start;
                     else
                         lenth=i-a.top();
                 }
