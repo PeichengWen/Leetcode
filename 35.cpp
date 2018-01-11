ca495d7d3c4cf4360395a42ce679b158950baea9
PeichengWen <pwen@troy.edu>
2018-01-11 15:13:02 -0600

    Wrong Answer

---
+++ 35.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int i=0;
+        int n = nums.size();
+        if(n==0) return 0;
+        int j = n-1;
+        while(i<j){
+            int mid = i+(j-i)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[mid]>target) j = mid;
+            else i=mid+1;
+        }
+        return (i);
+    }
+};
