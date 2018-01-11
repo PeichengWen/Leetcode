b7a4b755e206d43e33d9b373e011be8d8c530c74
PeichengWen <pwen@troy.edu>
2018-01-10 19:09:23 -0600

    accepted
    left move makes it efficient

--- 29.cpp
+++ 29.cpp
@@ -2,21 +2,26 @@ class Solution {
 public:
     int divide(int dividend, int divisor) {
         
-        if(divisor==0) return INT_MAX;
+        if(divisor==0||(dividend==INT_MIN&&divisor==-1)) return INT_MAX;
         
         long long res = 0;
         int i = 1;
         if((dividend<=0&&divisor>0)||(dividend>=0&&divisor<0)) i = -1;
-        long long end,sor;
-        end=abs(dividend);
-        sor = abs(divisor);
-        while(end>=sor){
-            res++;
-            end-=sor;
+        long long dend=0,sor=0;
+        dend=labs(dividend);
+        sor = labs(divisor);
+        while(dend>=sor){
+            long long temp=sor,multiple=1;
+            while(dend>=temp<<1){
+                temp<<=1;
+                multiple<<=1;
+            }
+            res+=multiple;
+            dend-=temp;
         }
         
-        if(i==-1){
-            return (0-res);}
+        if(i==-1)
+            return (0-res);
         else return res;
     }
 };
