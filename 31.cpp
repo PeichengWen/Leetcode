c8d0f6740e88864cea845e13ae794eea6261c346
PeichengWen <pwen@troy.edu>
2018-01-11 11:20:38 -0600

    Accept

--- 31.cpp
+++ 31.cpp
@@ -1,34 +1,19 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int temp=0;
-        int n=nums.size();
+        int n = nums.size();
         if(n==1) return;
-        if(n==2){
-            temp=nums[0];
-            nums[0]=nums[1];
-            nums[1]=temp;
-        }
-        int i=n-2;
-        
+        int i = nums.size()-2;
+        int j = nums.size()-1;
         for(;i>=0;i--){
-            if(nums[i+1]>nums[i]&&(i+2)!=n){
-                for(int i+)
-                    }else if(nums[i+1]>nums[i]){
-                        temp=nums[i+1];
-                        nums[i+1]=nums[i];
-                        nums[i]=temp;
-                        break;
-                    }
-        }
-        if(i==-1){
-            i=n-2;
-            for(;i>=0;i--){
-                temp=nums[i+1];
-                nums[i+1]=nums[i];
-                nums[i]=temp;
+            if(nums[i]<nums[i+1]){
+                for(;j>=i;j--){
+                    if(nums[j]>nums[i]) break;
+                }
+                swap(nums[i],nums[j]);
+                break;
             }
         }
-        return;
+        reverse(nums.begin()+i+1,nums.end());
     }
 };
