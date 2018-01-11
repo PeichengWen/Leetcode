6aaa03132c638bc101fb20fddb57c6902d6f4975
PeichengWen <pwen@troy.edu>
2018-01-11 14:19:51 -0600

    Accepted

--- 34.cpp
+++ 34.cpp
@@ -16,15 +16,15 @@ public:
         res.push_back(left);
         left = 0;
         right = n-1;
-        int rightb=0;
         while(left<right){
             int mid = left+(right-left)/2;
             if(nums[mid]<=target) left = mid+1;
             else right = mid;
-            rightb=left-1;
         }
+        if(nums[left]==target)
+            res.push_back(left);
+        else res.push_back(left-1);
         
-        res.push_back(rightb);
         return res;
         
         
